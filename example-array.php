55ec347d2aafbf56848ce52544937759ffb5b2ac
msarac61 <metin_@hotmail.com.tr>
2021-04-15 15:29:00 +0300

    Update

--- example-array.php
+++ example-array.php
@@ -1,5 +1,7 @@
 <?php
 
+	require_once( 'config.php' );
+
 	$box = array('fields'  => array(
 
 		array(
@@ -89,91 +91,5 @@
 		),	
 		
 	));
-		
-	function data($array) {
-
-		foreach($array as $val) {
-
-			switch ($val['type']) {
-				
-				case 'text':
-				echo '<div class="sare-fields">';
-				echo '<div class="sare-title">'.$val['label'].'</div>';
-				echo '<hr>';
-				echo '<input type="text" name="'.$val['id'].'" value="'.$val['standart'].'">';
-				echo '</div>';
-				break;
-				
-				case 'textarea':
-				echo '<div>';
-				echo '<div class="sare-title">'.$val['label'].'</div>';
-				echo '<hr>';
-				echo '<textarea type="text" name="'.$val['id'].'">'.$val['standart'].'</textarea>';
-				echo '</div>';			
-				break;
-				
-				case 'checkbox':
-				echo '<div class="sare-fields">';
-				echo '<div class="sare-title">'.$val['label'].'</div>';
-				echo '<hr>';
-				foreach($val['choices'] as $choices) {
-					
-					if($choices['value'] == $val['standart']) {
-						echo '<input name="'.$val['id'].'[]" type="checkbox" checked>'.$choices['label'].'';
-					} else {
-						echo '<input name="'.$val['id'].'[]" type="checkbox">'.$choices['label'].'';			
-					}
-					
-				}
-				echo '</div>';
-				break;
-				
-				case 'radio':
-				echo '<div class="sare-fields">';
-				echo '<div class="sare-title">'.$val['label'].'</div>';
-				echo '<hr>';
-				foreach($val['choices'] as $choices) {
-					
-					if($choices['value'] == $val['standart']) {
-						echo '<input name="'.$val['id'].'" type="radio" checked>'.$choices['label'].'';
-					} else {
-						echo '<input name="'.$val['id'].'" type="radio">'.$choices['label'].'';			
-					}	
-					
-				}
-				echo '</div>';						
-				break;
-				
-				case 'dropdown':
-				echo '<div class="sare-fields">';
-				echo '<div class="sare-title">'.$val['label'].'</div>';
-				echo '<hr>';
-				echo '<select name="'.$val['id'].'">';
-				foreach($val['choices'] as $choices) {
-					
-					if($choices['value'] == $val['standart']) {
-						echo '<option value="'.$choices['value'].'" selected>'.$choices['label'].'</option>';
-					} else {
-						echo '<option value="'.$choices['value'].'">'.$choices['label'].'</option>';
-					}
-					
-				}
-				echo '</select>';
-				echo '</div>';						
-				break;			
-				
-			}
-			
-		}
-
-	}
 
 ?>
-
-<form method="post">
-
-  <?php data($box['fields']); ?>
-
-  <input type="submit">
-  
-</form>
